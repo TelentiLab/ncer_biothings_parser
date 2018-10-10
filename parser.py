fa24d8af61ad29061045091d25ddb23c652c1121
Li Yin <liyin@scripps.edu>
2018-10-10 09:51:24 -0700

    Changed file URI to full dataset

--- parser.py
+++ parser.py
@@ -10,7 +10,7 @@ FILE_NOT_FOUND_ERROR = 'Cannot find input file: {}'   # error message constant
 # change following parameters accordingly
 data_schema = ('chrom', 'start', 'end', 'ncer_percentile')    # field names of the data
 source_name = 'ncer'   # source name that appears in the api response
-file_name = 'ncer_data'   # name of the file to read
+file_name = 'sliding10bp_window10bp_ncER_OMNI.txt'   # name of the file to read
 delimiter = '\t'    # the delimiter that separates each field
 
 
