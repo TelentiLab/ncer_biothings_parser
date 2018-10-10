58e3b69148ef651da9d63d5c97950c26b01715f7
Li Yin <liyin@scripps.edu>
2018-10-10 15:36:51 -0700

    Refactored data parsing logic

--- parser.py
+++ parser.py
@@ -8,9 +8,8 @@ from biothings.utils.dataload import open_anyfile, dict_sweep
 FILE_NOT_FOUND_ERROR = 'Cannot find input file: {}'   # error message constant
 
 # change following parameters accordingly
-data_schema = ('chrom', 'start', 'end', 'ncer_percentile')    # field names of the data
 source_name = 'ncer'   # source name that appears in the api response
-file_name = 'sliding10bp_window10bp_ncER_OMNI.txt'   # name of the file to read
+file_name = 'sample_data'   # name of the file to read
 delimiter = '\t'    # the delimiter that separates each field
 
 
@@ -27,22 +26,19 @@ def load_data(data_folder: str):
     # raise an error if file not found
     assert os.path.exists(input_file), FILE_NOT_FOUND_ERROR.format(input_file)
 
-    with open_anyfile(input_file) as file:
-        # Remove duplicated lines if any
-        lines = set(list(file))
-        # read and parse each line into a dict
-        reader = DictReader(lines, fieldnames=data_schema, delimiter=delimiter)
-        # access non existing keys will return an empty list by default
+    with open(file_name, 'r') as file:
         results = defaultdict(list)
-
-        for row in reader:  # start processing each lines of data (stored in dicts)
-            # construct id (e.g. chr1:g.678900_679000)
-            _id = '{chrom}:g.{start}_{end}'.format(chrom=row['chrom'], start=row['start'], end=row['end'])
-            # optional step: normalize data
-            variant = {k: unicodedata.normalize('NFKD', v) for k, v in row.items()}
-            # optional step: delete invalid fields within each dict
+        for line in file:
+            # read and parse each line into a dict
+            chrom, start, end, percentile = line.strip().split(delimiter)
+            _id = '{chrom}:g.{start}_{end}'.format(chrom=chrom, start=start, end=end)
+            variant = {
+                "chrom": chrom,
+                "start": start,
+                "end": end,
+                "percentile": percentile,
+            }
             variant = dict_sweep(variant, vals=['', 'null', 'N/A', None, [], {}])
-            # append dict to the result list using the _id
             results[_id].append(variant)
 
         for k, v in results.items():
