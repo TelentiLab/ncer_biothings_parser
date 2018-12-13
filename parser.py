1a5af7f7965f4b53c9d4bf334a40f42305c28889
Li Yin <liyin@scripps.edu>
2018-12-12 16:46:50 -0800

    Fixed chr key name

--- parser.py
+++ parser.py
@@ -2,15 +2,15 @@ import os
 import logging
 from biothings.utils.dataload import dict_sweep
 
-FILE_NOT_FOUND_ERROR = 'Cannot find input file: {}'   # error message constant
-FILE_LINES = 290366 # sample file lines
+FILE_NOT_FOUND_ERROR = 'Cannot find input file: {}'  # error message constant
+FILE_LINES = 290366  # sample file lines
 # FILE_LINES = 282932324
 
 # change following parameters accordingly
-source_name = 'ncer'   # source name that appears in the api response
-file_name = 'sample_data.txt'   # sample file
+source_name = 'ncer'  # source name that appears in the api response
+file_name = 'sample_data.txt'  # sample file
 # file_name = 'sliding10bp_window10bp_ncER_OMNI.txt'   # name of the file to read
-delimiter = '\t'    # the delimiter that separates each field
+delimiter = '\t'  # the delimiter that separates each field
 
 # configure logger
 logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', level=logging.INFO)
@@ -36,10 +36,11 @@ def load_data(data_folder: str):
         logger.info(f'start reading file: {file_name}')
         count = 0
         for line in file:
-            logger.info(f'reading line {count} ({(count / FILE_LINES * 100):.2f}%)')    # format to use 2 decimals
+            logger.info(f'reading line {count} ({(count / FILE_LINES * 100):.2f}%)')  # format to use 2 decimals
             count += 1
             # read and parse each line into a dict
             chrom, start, end, percentile = line.strip().split(delimiter)
+            chrom = chrom.replace('chr', '')
             _id = f'chr{chrom}:g.{start}_{end}'
             # enforce data type
             variant = {
