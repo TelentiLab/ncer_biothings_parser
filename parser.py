1065fb9e096c922a77f24727f11fa6c77b7b85ea
Li Yin <liyin@scripps.edu>
2019-04-09 16:20:31 -0700

    Added estimated time logging

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
 import os
 import logging
+import datetime
+import time
 
 FILE_NOT_FOUND_ERROR = 'Cannot find input file: {}'  # error message constant
 FILE_LINES = 282932324
@@ -33,12 +35,16 @@ def load_data(data_folder: str):
         raise FileExistsError(FILE_NOT_FOUND_ERROR.format(input_file))
 
     with open(input_file, 'r') as file:
+        start_time = time.time()
         logger.info(f'start reading file: {file_name}')
         count = 0
         skipped = []
         for line in file:
             count += 1
-            logger.info(f'reading line {count} ({(count / FILE_LINES * 100):.2f}%)')  # format to use 2 decimals
+            ratio = count / FILE_LINES
+            time_left = datetime.timedelta(seconds=(time.time() - start_time) * (1 - ratio) / ratio)
+            # format to use 2 decimals for progress
+            logger.info(f'reading line {count} ({(count / FILE_LINES * 100):.2f}%), estimated time left: {time_left}')
             # read and parse each line into a dict
             try:
                 chrom, start, end, percentile = line.strip().split(delimiter)
